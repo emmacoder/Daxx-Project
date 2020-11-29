d12199a7a4ffd7096ad21c96efdc75d7bc7b386c
emmacoder <emanuelchale@gmail.com>
2020-11-29 17:39:40 +0300

    Sunday

---
+++ test.js
@@ -0,0 +1,135 @@
+/*
+var delTimeName ="Delivery time";
+var avg =" AVG ";
+var delTime = 30;
+var delTimeMinutes =" mins."
+
+alert (delTimeName + avg + delTime + delTimeMinutes);
+
+*/
+/*
+function saySomething (num,str) {
+
+    var add = num + str;
+    alert (add)
+}
+saySomething (455," program to write");
+
+*/
+/*
+var number1 = 7;
+var number2 = 4;
+
+var number1 = number1 + number2;
+
+document.write (number1);
+*/
+
+/*
+var food ="apple";
+
+if (food=="apple") {
+    document.write ("we ate somefood and specifically an apple")
+
+}
+
+else if (food == "Meat") {
+
+}
+
+else {
+    document.write ("we ate something else")
+}
+*/
+
+/*document.write ("this is the statement")
+
+var delTime =" Delivery time"
+var avg =" AVG: "
+var delTimeValue = 35
+
+
+document.write (delTime + avg + delTimeValue);
+*/
+
+/*var food ="meat"
+
+if (food =="apple") {
+
+    document.write ("we ate some apples")
+}
+
+else if (food=="meat") {
+    document.write ("we ate some junk")
+}
+
+else {
+    document.write ("we ate some embe")
+}
+*/
+
+/*var i = 1;
+while (i < 5 ) {
+
+    document.write ("This is a while loop");
+
+    i++;
+}
+*/
+
+
+/*for (var i = 1; i < 5; i++ ) {
+
+    document.write ("This is a while loop");
+
+}
+*/
+
+/*function batting (player, distance) {
+
+    if (distance <= 350 && distance > 0){
+        document.write (player + " hit the ball");
+    }
+    else if (distance <= 0) {
+        document.write (player + "STRUCK OUT");
+    }
+    else {
+        document.write (player + " hit a home run")
+    }
+}
+
+batting ("Steve ", 0);
+*/
+
+
+/*function add(x,y) {
+
+    result = x + y;
+    return result;
+}
+
+var theResult = add (5,5);
+document.write (theResult); */
+
+
+/*function batting (player,distance) {
+
+    var more = player + " hit the ball " + distance + " feet";
+    document.write(more);
+
+}
+
+batting ("Steve", 351); */
+
+
+// ARRAYS
+
+var roads = ['stone', 'dirt', 'cement', 'tar',];
+
+roads[1] = "coblestone";
+
+var roadTravelled = roads[1];
+
+document.write(roadTravelled);
+
+alert ('Hello Madale')
